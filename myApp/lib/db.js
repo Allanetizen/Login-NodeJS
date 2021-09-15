5e74c5328463f5ee6221e39efecc1b92d121cb43
Allanetizen <allankosgei747@gmail.com>
2021-09-15 11:40:07 +0300

    all files

--- myApp/lib/db.js
+++ myApp/lib/db.js
@@ -2,6 +2,7 @@ var mysql=require('mysql');
  var connection=mysql.createConnection({
    host:'localhost',
    user:'root',
+   port: 8081,
    password:'your password',
    database:'logsystem'
  });
